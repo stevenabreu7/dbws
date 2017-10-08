efe61a4679ed6b5cb1344f444444236ac87f7c34
Matius Sulung Chairani <matiusulung@hotmail.com>
2017-10-08 19:03:03 +0200

    Updates sql/database.sql
    Auto commit by GitBook Editor

--- sql/database.sql
+++ sql/database.sql
@@ -3,144 +3,202 @@ CREATE DATABASE dbws;
 USE dbws;
 
 CREATE TABLE Residence (
-  rid INTEGER PRIMARY KEY,
-  name VARCHAR(20)
+	rid INT,
+	name VARCHAR(20), -- College name
+	PRIMARY KEY (rid)
 );
 
 CREATE TABLE Category (
-  catid INTEGER PRIMARY KEY,
-  name VARCHAR(30)
+	catid INT,
+	name VARCHAR(30),
+	PRIMARY KEY (catid)
 );
 
 CREATE TABLE Location (
-  lid INTEGER PRIMARY KEY,
-  name VARCHAR(30)
+	locid INT,
+	name VARCHAR(30),
+	PRIMARY KEY (locid)
 );
 
-CREATE TABLE EventDate (
-  -- rethink this one! multiple with same primary key!
-  recid INTEGER
+CREATE TABLE RecurringSingle (
+	rsid INT,
+	recid INT,
+	day DATE,
+	starttime TIME,
+	endtime TIME,
+	PRIMARY KEY (rsid),
+	FOREIGN KEY (recid) REFERENCES RecurringEvent(recid)
 );
 
 CREATE TABLE User (
-  uid INTEGER PRIMARY KEY,
-  firstname VARCHAR(20),
-  lastname VARCHAR(30),
-  email VARCHAR(30),
-  activated BOOLEAN,
-  joined DATE,
-  rid INTEGER,
-  FOREIGN KEY (rid) REFERENCES Residence(rid)
+	uid INT,
+	firstname VARCHAR(30),
+	lastname VARCHAR(30),
+	email VARCHAR(30),
+	activated BOOLEAN,
+	joined DATE,
+	rid INT,
+	PRIMARY KEY (uid),
+	FOREIGN KEY (rid) REFERENCES Residence(rid)
 );
 
 CREATE TABLE Admin (
-  uid INTEGER PRIMARY KEY,
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	uid INT,
+	PRIMARY KEY (uid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE Student (
-  uid INTEGER PRIMARY KEY,
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	uid INT,
+	major VARCHAR(30),
+	rid VARCHAR (20),
+	PRIMARY KEY (uid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE Staff (
-  uid INTEGER PRIMARY KEY,
-  department VARCHAR(30),
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	uid INT,
+	department VARCHAR(30),
+	PRIMARY KEY (uid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE Professor (
-  uid INTEGER PRIMARY KEY,
-  research VARCHAR(30),
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	uid INT,
+	research_area VARCHAR(30),
+	PRIMARY KEY (uid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE Notification (
-  nid INTEGER PRIMARY KEY,
-  uid INTEGER,
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	nid INT,
+	uid INT,
+	Message VARCHAR(256),
+	PRIMARY KEY (nid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE InviteNotification (
-  nid INTEGER PRIMARY KEY
+	nid INT,
+	PRIMARY KEY (nid),
+	FOREIGN KEY (nid) REFERENCES Notification(nid)
 );
 
 CREATE TABLE RequestNotification (
-  nid INTEGER PRIMARY KEY
+	nid INT,
+	PRIMARY KEY (nid),
+	FOREIGN KEY (nid) REFERENCES Notification(nid)
 );
 
-CREATE TABLE UserGroup (
-  gid INTEGER PRIMARY KEY,
-  name VARCHAR(30),
-  max INTEGER
+-- Groups can be official or unofficial groups i.e. CS Club, J-Capella, or Squad
+	CREATE TABLE UserGroup (
+	gid INT,
+	name VARCHAR(30),
+	max INT, -- Maximum number of members in a group
+	PRIMARY KEY (gid)
 );
 
 CREATE TABLE UserGroupMembership (
-  gid INTEGER,
-  uid INTEGER,
-  PRIMARY KEY (gid, uid),
-  FOREIGN KEY (gid) REFERENCES UserGroup(gid),
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	gid INT,
+	uid INT,
+	PRIMARY KEY (gid, uid),
+	FOREIGN KEY (gid) REFERENCES UserGroup(gid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE UserGroupOwner (
-  gid INTEGER,
-  uid INTEGER,
-  PRIMARY KEY (gid, uid),
-  FOREIGN KEY (gid) REFERENCES UserGroup(gid),
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	gid INT,
+	uid INT,
+	PRIMARY KEY (gid, uid),
+	FOREIGN KEY (gid) REFERENCES UserGroup(gid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE Event (
-  eid INTEGER PRIMARY KEY,
-  name VARCHAR(40),
-  public BOOLEAN,
-  gid INTEGER,
-  lid INTEGER,
-  catid INTEGER,
-  FOREIGN KEY (gid) REFERENCES UserGroup(gid),
-  FOREIGN KEY (lid) REFERENCES Location(lid),
-  FOREIGN KEY (catid) REFERENCES Category(catid)
+	eid INT,
+	name VARCHAR(40),
+	public BOOLEAN,
+	gid INT,
+	locid INT,
+	catid INT,
+	max INT, -- Maximum number of people in an event
+	PRIMARY KEY (eid),
+	FOREIGN KEY (gid) REFERENCES UserGroup(gid),
+	FOREIGN KEY (lid) REFERENCES Location(lid),
+	FOREIGN KEY (catid) REFERENCES Category(catid)
 );
 
 CREATE TABLE OneTimeEvent (
-  eid INTEGER PRIMARY KEY,
-  day INTEGER,
-  starttime DATE,
-  endtime DATE,
-  FOREIGN KEY (eid) REFERENCES Event(eid)
+	eid INT,
+	day DATE,
+	starttime TIME,
+	endtime TIME,
+	PRIMARY KEY (eid),
+	FOREIGN KEY (eid) REFERENCES Event(eid)
 );
 
 CREATE TABLE RecurringEvent (
-  reid INTEGER PRIMARY KEY,
-  eid INTEGER UNIQUE,
-  times INTEGER,
-  FOREIGN KEY (eid) REFERENCES Event(eid)
+	recid INT,
+	eid INT UNIQUE,
+	occurrences INT,
+	PRIMARY KEY(recid),
+	FOREIGN KEY (eid) REFERENCES Event(eid)
 );
 
 CREATE TABLE Organizer (
-  uid INTEGER PRIMARY KEY,
-  eid INTEGER UNIQUE,
-  FOREIGN KEY (uid) REFERENCES User(uid),
-  FOREIGN KEY (eid) REFERENCES Event(eid)
+	uid INT,
+	eid INT,
+	PRIMARY KEY (uid, eid),
+	FOREIGN KEY (uid) REFERENCES User(uid),
+	FOREIGN KEY (eid) REFERENCES Event(eid)
 );
 
 CREATE TABLE Invite (
-  iid INTEGER PRIMARY KEY,
-  uid INTEGER,
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	iid INT,
+	uid INT,
+	message VARCHAR(100),
+	decision BOOLEAN,
+	PRIMARY KEY (iid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
 );
 
 CREATE TABLE EventInvite (
-  iid INTEGER PRIMARY KEY,
-  eid INTEGER,
-  FOREIGN KEY (iid) REFERENCES Invite(iid),
-  FOREIGN KEY (eid) REFERENCES Event(eid)
+	iid INT,
+	eid INT,
+	PRIMARY KEY (iid),
+	FOREIGN KEY (iid) REFERENCES Invite(iid),
+	FOREIGN KEY (eid) REFERENCES Event(eid)
 );
 
 CREATE TABLE UserGroupInvite (
-  iid INTEGER PRIMARY KEY,
-  uid INTEGER,
-  FOREIGN KEY (iid) REFERENCES Invite(iid),
-  FOREIGN KEY (uid) REFERENCES User(uid)
+	iid INT,
+	gid INT,
+	PRIMARY KEY (iid),
+	FOREIGN KEY (iid) REFERENCES Invite(iid),
+	FOREIGN KEY (gid) REFERENCES User(gid)
+);
+
+CREATE TABLE EventAttendance (
+	uid INT,
+	eid INT,
+	PRIMARY KEY (uid, eid),
+	FOREIGN KEY (uid) REFERENCES User (uid),
+	FOREIGN KEY (eid) REFERENCES Event (eid)
+);
+
+CREATE TABLE Request (
+	reqid INT,
+	uid INT,
+	message VARCHAR(256),
+	decision BOOL,
+	PRIMARY KEY (reqid),
+	FOREIGN KEY (uid) REFERENCES User(uid)
+);
+
+CREATE TABLE EventRequest (
+	reqid INT,
+	eid INT,
+	PRIMARY KEY (reqid),
+	FOREIGN KEY (reqid) REFERENCES Request(reqid),
+	FOREIGN KEY (eid) REFERENCES Event(eid)
 );
